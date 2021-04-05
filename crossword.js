be967c5b830961e7440f40d28f6b9273c05ccb8b
Parth-Prajapati572 <81975218+Parth-Prajapati572@users.noreply.github.com>
2021-04-05 20:24:54 +0530

    Add files via upload

---
+++ crossword.js
@@ -0,0 +1,492 @@
+function keyCode(event) {
+    /*console.log("1")*/
+    var m001 = names001.value; var m002 = names002.value; var m003 = names003.value; var m004 = names004.value; var m005 = names005.value; 
+    var m006 = names006.value; var m007 = names007.value; var m008 = names008.value; var m009 = names009.value; var m012 = names012.value; 
+
+    var m016 = names016.value; var m017 = names017.value; var m018 = names018.value; var m019 = names019.value; var m020 = names020.value;
+    var m026 = names026.value; var m027 = names027.value; 
+
+    var m031 = names031.value; var m032 = names032.value; var m033 = names033.value; var m034 = names034.value; var m035 = names035.value; 
+    var m036 = names036.value; var m037 = names037.value; var m041 = names041.value; var m042 = names042.value; var m045 = names045.value;
+
+    var m046 = names046.value; var m049 = names049.value; var m056 = names056.value; var m057 = names057.value; var m058 = names058.value; 
+    var m060 = names060.value;
+
+    var m062 = names062.value; var m064 = names064.value; var m071 = names071.value; var m072 = names072.value; var m073 = names073.value; 
+    var m075 = names075.value; 
+
+    var m077 = names077.value; var m079 = names079.value; var m080 = names080.value; var m081 = names081.value; var m082 = names082.value; 
+    var m083 = names083.value; var m085 = names085.value; var m086 = names086.value; var m087 = names087.value; var m088 = names088.value; 
+    var m089 = names089.value; var m090 = names090.value;
+
+    var m091 = names091.value; var m092 = names092.value; var m094 = names094.value; var m098 = names098.value; var m103 = names130.value;
+    var m105 = names105.value; 
+    
+    var m107 = names107.value; var m111 = names111.value; var m113 = names113.value; var m114 = names114.value; var m118 = names118.value; 
+    
+    var m122 = names122.value; var m126 = names126.value; var m128 = names128.value; var m129 = names129.value; var m130 = names130.value;
+    var m133 = names133.value; 
+    
+    var m137 = names137.value; var m141 = names141.value; var m143 = names143.value; var m144 = names144.value; var m148 = names148.value; 
+    
+    var m152 = names152.value; var m155 = names155.value; var m156 = names156.value; var m157 = names157.value; var m158 = names158.value; 
+    var m159 = names159.value; var m163 = names163.value; 
+
+    var m167 = names167.value; var m171 = names171.value; var m172 = names172.value; var m174 = names174.value; 
+
+    var m182 = names182.value;
+
+   
+    var w001 = 0; var w002 = 0; var w003 = 0; var w004 = 0; var w005 = 0; var w006 = 0; var w007 = 0; var w008 = 0; var w009 = 0; var w012 = 0; 
+
+    var w016 = 0; var w017 = 0; var w018 = 0; var w019 = 0; var w020 = 0; var w026 = 0; var w027 = 0; 
+
+    var w031 = 0; var w032 = 0; var w033 = 0; var w034 = 0; var w035 = 0; var w036 = 0; var w037 = 0; var w041 = 0; var w042 = 0; var w045 = 0;
+
+    var w046 = 0; var w049 = 0; var w056 = 0; var w057 = 0; var w058 = 0; var w060 = 0;
+
+    var w062 = 0; var w064 = 0; var w071 = 0; var w072 = 0; var w073 = 0; var w075 = 0; 
+
+    var w077 = 0; var w079 = 0; var w080 = 0; var w081 = 0; var w082 = 0; var w083 = 0; var w085 = 0; var w086 = 0; var w087 = 0; var w088 = 0; 
+    var w089 = 0; var w090 = 0;
+
+    var w091 = 0; var w092 = 0; var w094 = 0; var w098 = 0; var w103 = 0; var w105 = 0; 
+    
+    var w107 = 0; var w111 = 0; var w113 = 0; var w114 = 0; var w118 = 0; 
+    
+    var w122 = 0; var w126 = 0; var w128 = 0; var w129 = 0; var w130 = 0; var w133 = 0; 
+    
+    var w137 = 0; var w141 = 0; var w143 = 0; var w144 = 0; var w148 = 0; 
+    
+    var w152 = 0; var w155 = 0; var w156 = 0; var w157 = 0; var w158 = 0; var w159 = 0; var w163 = 0; 
+
+    var w167 = 0; var w171 = 0; var w172 = 0; var w174 = 0; 
+
+    var w182 = 0;
+    
+    var b = event.keyCode;
+
+
+    if (m001 == "u") {
+        console.log("u")
+        w001 = 1;
+        document.getElementById("names001").value = m001;
+    }
+    if (m002 == "n") {
+        w002 = 1;
+        document.getElementById("names002").value = m002;
+    }
+    if (m003 == "l") {
+        w003 = 1;
+        document.getElementById("names003").value = m003;
+    }
+    
+    if (m004 == "i") {
+        w004 = 1;
+        document.getElementById("names004").value = m004;
+    }
+    if (m005 == "m") {
+        w005 = 1;
+        document.getElementById("names005").value = m005;
+    }
+    if (m006 == "i") {
+        w006 = 1;
+        document.getElementById("names006").value = m006;
+    }
+    
+    if (m007 == "t") {
+        w007 = 1;
+        document.getElementById("names007").value = m007;
+    }
+    if (m008 == "e") {
+        w008 = 1;
+        document.getElementById("names008").value = m008;
+    }
+    if (m009 == "d") {
+        w009 = 1;
+        document.getElementById("names009").value = m009;
+    }
+    
+    if (m012 == "i") {
+        w012 = 1;
+        document.getElementById("names012").value = m012;
+    }
+    
+    if (m016 == "s") {
+        w016 = 1;
+        document.getElementById("names016").value = m016;
+    }
+    if (m017 == "t") {
+        w017 = 1;
+        document.getElementById("names017").value = m017;
+    }
+    if (m018 == "a") {
+        w018 = 1;
+        document.getElementById("names018").value = m018;
+    }
+    if (m019 == "c") {
+        w019 = 1;
+        document.getElementById("names019").value = m019;
+    }
+    if (m020 == "k") {
+        w020 = 1;
+        document.getElementById("names020").value = m020;
+    }
+    
+    if (m026 == "w") {
+        w026 = 1;
+        document.getElementById("names026").value = m026;
+    }
+    if (m027 == "m") {
+        w027 = 1;
+        document.getElementById("names027").value = m027;
+    }
+    
+    if (m031 == "e") {
+        w031 = 1;
+        document.getElementById("names031").value = m031;
+    }
+    if (m032 == "x") {
+        w032 = 1;
+        document.getElementById("names032").value = m032;
+    }
+    if (m033 == "e") {
+        w033 = 1;
+        document.getElementById("names033").value = m033;
+    }
+    if (m034 == "c") {
+        w034 = 1;
+        document.getElementById("names034").value = m034;
+    }
+    if (m035 == "u") {
+        w035 = 1;
+        document.getElementById("names035").value = m035;
+    }
+    if (m036 == "t") {
+        w036 = 1;
+        document.getElementById("names036").value = m036;
+    }
+    if (m037 == "e") {
+        w037 = 1;
+        document.getElementById("names037").value = m037;
+    }
+    
+    if (m041 == "r") {
+        w041 = 1;
+        document.getElementById("names041").value = m041;
+    }
+    
+    if (m042 == "p") {
+        w042 = 1;
+        document.getElementById("names042").value = m042;
+    }
+    
+    if (m045 == "b") {
+        w045 = 1;
+        document.getElementById("names045").value = m045;
+    }
+    
+    if (m046 == "r") {
+        w046 = 1;
+        document.getElementById("names046").value = m046;
+    }
+    
+    if (m049 == "h") {
+        w049 = 1;
+        document.getElementById("names049").value = m049;
+    }
+    
+    if (m056 == "i") {
+        w056 = 1;
+        document.getElementById("names056").value = m056;
+    }
+    
+    if (m057 == "o") {
+        w057 = 1;
+        document.getElementById("names057").value = m057;
+    }
+    
+    if (m058 == "r") {
+        w058 = 1;
+        document.getElementById("names058").value = m058;
+    }
+    
+    if (m060 == "a") {
+        w060 = 1;
+        document.getElementById("names060").value = m060;
+    }
+    
+    
+    if (m062 == "c") {
+        w062 = 1;
+        document.getElementById("names062").value = m062;
+    }
+    
+    if (m064 == "m") {
+        w064 = 1;
+        document.getElementById("names064").value = m064;
+    }
+    
+    if (m071 == "t") {
+        w071 = 1;
+        document.getElementById("names071").value = m071;
+    }
+    
+    if (m072 == "r") {
+        w072 = 1;
+        document.getElementById("names072").value = m072;
+    }
+    
+    if (m073 == "e") {
+        w073 = 1;
+        document.getElementById("names073").value = m073;
+    }
+    
+    if (m075 == "t") {
+        w075 = 1;
+        document.getElementById("names075").value = m075;
+    }
+    
+    if (m077 == "a") {
+        w077 = 1;
+        document.getElementById("names077").value = m077;
+    }
+    
+    if (m079 == "o") {
+        w079 = 1;
+        document.getElementById("names079").value = m079;
+    }
+    if (m080 == "c") {
+        w080 = 1;
+        document.getElementById("names080").value = m080;
+    }
+    if (m081 == "t") {
+        w081 = 1;
+        document.getElementById("names081").value = m081;
+    }
+    if (m082 == "a") {
+        w082 = 1;
+        document.getElementById("names082").value = m082;
+    }
+    if (m083 == "l") {
+        w083 = 1;
+        document.getElementById("names083").value = m083;
+    }
+    
+    if (m085 == "m") {
+        w085 = 1;
+        document.getElementById("names085").value = m085;
+    }
+    if (m086 == "e") {
+        w086 = 1;
+        document.getElementById("names086").value = m086;
+    }
+    if (m087 == "t") {
+        w087 = 1;
+        document.getElementById("names087").value = m087;
+    }
+    if (m088 == "s") {
+        w088 = 1;
+        document.getElementById("names088").value = m088;
+    }
+    if (m089 == "y") {
+        w089 = 1;
+        document.getElementById("names089").value = m089;
+    }
+    if (m090 == "s") {
+        w090 = 1;
+        document.getElementById("names090").value = m090;
+    }
+    if (m091 == "p") {
+        w091 = 1;
+        document.getElementById("names091").value = m091;
+    }
+    if (m092 == "s") {
+        w092 = 1;
+        document.getElementById("names092").value = m092;
+    }
+    
+    if (m094 == "d") {
+        w094 = 1;
+        document.getElementById("names094").value = m094;
+    }
+    
+    if (m098 == "e") {
+        w098 = 1;
+        document.getElementById("names098").value = m098;
+    }
+    
+    if (m103 == "p") {
+        w103 = 1;
+        document.getElementById("names103").value = m103;
+    }
+    
+    if (m105 == "f") {
+        w105 = 1;
+        document.getElementById("names105").value = m105;
+    }
+    
+    if (m107 == "c") {
+        w107 = 1;
+        document.getElementById("names107").value = m107;
+    }
+    
+    if (m111 == "t") {
+        w111 = 1;
+        document.getElementById("names111").value = m111;
+    }
+    
+    if (m113 == "t") {
+        w113 = 1;
+        document.getElementById("names113").value = m113;
+    }
+    
+    if (m114 == "m") {
+        w114 = 1;
+        document.getElementById("names114").value = m114;
+    }
+    
+    if (m118 == "o") {
+        w118 = 1;
+        document.getElementById("names118").value = m118;
+    }
+    
+    if (m122 == "a") {
+        w122 = 1;
+        document.getElementById("names122").value = m122;
+    }
+    
+    if (m126 == "l") {
+        w126 = 1;
+        document.getElementById("names126").value = m126;
+    }
+    
+    if (m128 == "t") {
+        w128 = 1;
+        document.getElementById("names128").value = m128;
+    }
+    
+    if (m129 == "o") {
+        w129 = 1;
+        document.getElementById("names129").value = m129;
+    }
+    
+    if (m130 == "p") {
+        w130 = 1;
+        document.getElementById("names130").value = m130;
+    }
+    
+    if (m133 == "n") {
+        w133 = 1;
+        document.getElementById("names133").value = m133;
+    }
+    
+    if (m137 == "d") {
+        w137 = 1;
+        document.getElementById("names137").value = m137;
+    }
+    
+    if (m141 == "u") {
+        w141 = 1;
+        document.getElementById("names141").value = m141;
+    }
+    
+    if (m143 == "e") {
+        w143 = 1;
+        document.getElementById("names143").value = m143;
+    }
+    
+    if (m144 == "u") {
+        w144 = 1;
+        document.getElementById("names144").value = m144;
+    }
+    
+    if (m148 == "s") {
+        w148 = 1;
+        document.getElementById("names148").value = m148;
+    }
+    
+    if (m152 == "i") {
+        w152 = 1;
+        document.getElementById("names152").value = m152;
+    }
+    
+    if (m155 == "d") {
+        w155 = 1;
+        document.getElementById("names155").value = m155;
+    }
+    
+    if (m156 == "a") {
+        w156 = 1;
+        document.getElementById("names156").value = m156;
+    }
+    
+    if (m157 == "e") {
+        w157 = 1;
+        document.getElementById("names157").value = m157;
+    }
+    
+    if (m158 == "r") {
+        w158 = 1;
+        document.getElementById("names158").value = m158;
+    }
+    
+    if (m159 == "n") {
+        w159 = 1;
+        document.getElementById("names159").value = m159;
+    }
+    
+    if (m163 == "e") {
+        w163 = 1;
+        document.getElementById("names163").value = m163;
+    }
+    
+    if (m167 == "n") {
+        w167 = 1;
+        document.getElementById("names167").value = m167;
+    }
+    
+    if (m171 == "f") {
+        w171 = 1;
+        document.getElementById("names171").value = m171;
+    }
+    
+    if (m172 == "g") {
+        w172 = 1;
+        document.getElementById("names172").value = m172;
+    }
+    
+    if (m174 == "t") {
+        w174 = 1;
+        document.getElementById("names174").value = m174;
+    }
+    
+    if (m182 == "g") {
+        w182 = 1;
+        document.getElementById("names182").value = m182;
+    }
+    
+
+    var q = w001 + w002 + w003 + w004 + w005 + w006 + w007 + w008 + w009 + w012 + 
+            w016 + w017 + w018 + w019 + w020 + w026 + w027 + 
+            w031 + w032 + w033 + w034 + w035 + w036 + w037 + w041 + w042 + w045 +
+            w046 + w049 + w056 + w057 + w058 + w060 +
+            w062 + w064 + w071 + w072 + w073 + w075 + 
+            w077 + w079 + w080 + w081 + w082 + w083 + w085 + w086 + w087 + w088 + w089 + w090 +
+            w091 + w092 + w094 + w098 + w103 + w105 +     
+            w107 + w111 + w113 + w114 + w118 +     
+            w122 + w126 + w128 + w129 + w130 + w133 +     
+            w137 + w141 + w143 + w144 + w148 +     
+            w152 + w155 + w156 + w157 + w158 + w159 + w163 + 
+            w167 + w171 + w172 + w174 + 
+            w182 ;
+    
+    console.log(q)
+    document.getElementById("score001").innerHTML = "Number of correct letters: " + q + " out of 85.";
+    if (q == 85) {
+        document.getElementById("next001").innerHTML = "Congratulations! You have completed this crossword puzzle !!!";
+    }
+
+}
+
+    
